635709c7829afba5a3e3a57c584814f3ba621a95
alexandre-mouysset <12258@holbertonstudents.com>
2026-04-10 20:16:29 +0200

    feat: function that takes a float as argument and returns a function that multiplies a float by multiplier

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""Create a multiplier function"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Return function that multiplies by multiplier"""
+    def multiply(x: float) -> float:
+        """Multiply x by multiplier"""
+        return (x * multiplier)
+    return multiply
