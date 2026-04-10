448c7d484a03698ee685bbf8dfede99b5cd0cfd0
alexandre-mouysset <12258@holbertonstudents.com>
2026-04-10 19:40:59 +0200

    feat: function that return a concatenated string

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""Concatenate two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return concatenation of str1 and str2."""
+    total = str1 + str2
+    return total
