d195c4a5f917b1a6e179787e12acc18d12fabeac
alexandre-mouysset <12258@holbertonstudents.com>
2026-04-10 19:56:12 +0200

    feat: function that takes a list of floats as argument and returns their sum as a float

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""Sum list of floats."""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Return sum of all floats in list."""
+    total = sum(input_list)
+    return float(total)
