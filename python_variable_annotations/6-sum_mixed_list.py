260d5580d55b3dc4502f2c408b1ac8396e489bf0
alexandre-mouysset <12258@holbertonstudents.com>
2026-04-10 20:03:16 +0200

    fixed order inside List

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """Return sum of mixed int/float list as float."""
     total = sum(mxd_list)
     return float(total)
