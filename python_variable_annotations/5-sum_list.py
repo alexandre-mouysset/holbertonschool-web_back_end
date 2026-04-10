27aa0fc762a099908e762a05eb8e1f237285faea
alexandre-mouysset <12258@holbertonstudents.com>
2026-04-10 19:58:09 +0200

    added library

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Sum list of floats."""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Return sum of all floats in list."""
     total = sum(input_list)
     return float(total)
