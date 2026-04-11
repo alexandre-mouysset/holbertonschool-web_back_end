8bbd9ddeede018d688e96e5363e0f169e97fb7f6
alexandre-mouysset <12258@holbertonstudents.com>
2026-04-11 11:40:41 +0200

    feat: function that return The list of the delays should be in ascending order

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+"""Run multiple wait_random coroutines concurrently."""
+
+from typing import List
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Return delays from concurrent wait_random calls in completion order."""
+
+    tasks = []
+    results = []
+
+    for i in range(n):
+        tasks.append(wait_random(max_delay))
+
+    for task in asyncio.as_completed(tasks):
+        result = await task
+        results.append(result)
+
+    return results
