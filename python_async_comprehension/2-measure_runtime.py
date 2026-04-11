1521cc8d291b8536038dabdd4f9a594746ac0ee2
alexandre-mouysset <12258@holbertonstudents.com>
2026-04-11 23:34:43 +0200

    feat: implement measure_runtime function to calculate total runtime of async comprehensions

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""Measure total runtime of concurrent async comprehensions"""
+
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Run four async_comprehension calls concurrently and return runtime"""
+
+    start = time.time()
+    tasks = [async_comprehension() for i in range(4)]
+    await asyncio.gather(*tasks)
+    end = time.time()
+    return (end - start)
