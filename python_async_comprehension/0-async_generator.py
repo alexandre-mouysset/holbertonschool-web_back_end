32629372b1de33a2e0a12021f2d7af4e234ae86e
alexandre-mouysset <12258@holbertonstudents.com>
2026-04-11 23:15:19 +0200

    feat: specify return type for async_generator function

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """Yield 10 random float values, one per second."""
     for i in range(10):
         await asyncio.sleep(1)
