e1d9fd2396a1a44f91f981fd589b5ef52213ca7a
alexandre-mouysset <12258@holbertonstudents.com>
2026-04-24 21:11:01 +0200

    feat: add update_topics function to update topic lists in MongoDB

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Script tha update the topic lists"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Update topics with topics list"""
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}})
