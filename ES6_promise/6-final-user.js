466c9c1e535007b407da5d0c7bfbe5b211866803
alexandre-mouysset <12258@holbertonstudents.com>
2026-05-09 22:20:27 +0200

    style: format code for consistency in 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,18 @@
-import signUpUser from "./4-user-promise.js";
-import uploadPhoto from "./5-photo-reject.js";
+import signUpUser from "./4-user-promise.js"
+import uploadPhoto from "./5-photo-reject.js"
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const results = await Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName)
-  ]);
+  ])
 
-  return results.map((item) => ({
-    status: item.status,
-    value: item.value || item.reason
-  }));
+  return results.map(item => {
+    return {
+      status: item.status,
+      value: item.status === "fulfilled"
+        ? item.value
+        : item.reason
+    }
+  })
 }
