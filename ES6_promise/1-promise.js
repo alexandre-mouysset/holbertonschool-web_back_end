7992156d921c963fd064158adf06d6e27fe12067
alexandre-mouysset <12258@holbertonstudents.com>
2026-05-09 21:56:59 +0200

    feat: implement getFullResponseFromAPI function to handle API responses

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,9 @@
+export default async function getFullResponseFromAPI(success) {
+	if (success) {
+		return {
+			status: 200,
+			body: "Success"
+		}
+	}
+	throw new Error("The fake API is not working currently")
+}
