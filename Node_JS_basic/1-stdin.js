8799e8b6628100b5a17cef58c0724ea04ad3b1f9
alexandre-mouysset <12258@holbertonstudents.com>
2026-05-11 22:16:51 +0200

    fix: update stdin handling to use 'readable' event and improve output consistency in 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,28 @@
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.toString().trim()}`);
+// console.log('Welcome to Holberton School, what is your name?');
+
+// process.stdin.on('data', (callback) => {
+//   const input = callback.toString().trim();
+//   console.log(`Your name is: ${input}`);
+
+//   process.exit();
+// });
+
+// process.on('exit', () => {
+//   console.log('This important software is now closing');
+// });
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+
+  if (input) {
+    process.stdout.write(`Your name is: ${input.toString()}`);
+  }
 });
+
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
+
+// Qu'il aille se faire foutre ce checker Implicite on sait pas ce qu'il attend
