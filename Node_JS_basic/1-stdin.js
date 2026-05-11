a7d63ddc0add6c9eb1e5433aaa2e5722ad6c21f7
alexandre-mouysset <12258@holbertonstudents.com>
2026-05-11 21:57:28 +0200

    fix: simplify data handling in stdin event in 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
-
-process.stdin.on('data', (data) => {
-  const input = data.toString().trim();
-  console.log(`Your name is: ${input}`);
+process.stdin.on('data', (callback) => {
+  console.log(`Your name is: ${callback.toString().trim()}`);
 });
-
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
