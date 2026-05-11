6b4ed4e5fdf8698b6830b5a887982ab11a17b616
alexandre-mouysset <12258@holbertonstudents.com>
2026-05-11 21:58:07 +0200

    fix: correct parameter name in stdin data handling in 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (callback) => {
-  console.log(`Your name is: ${callback.toString().trim()}`);
+process.stdin.on('data', (data) => {
+  console.log(`Your name is: ${data.toString().trim()}`);
 });
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
