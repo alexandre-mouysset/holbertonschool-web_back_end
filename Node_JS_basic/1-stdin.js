813dac67ff7cc50e347c95a144429d24e1c6d35e
alexandre-mouysset <12258@holbertonstudents.com>
2026-05-11 21:48:16 +0200

    fix: move exit handling to 'end' event in 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,6 +3,8 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (callback) => {
   const input = callback.toString().trim();
   console.log(`Your name is: ${input}`);
-  console.log('This important software is now closing');
-  process.exit();
 });
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+})
