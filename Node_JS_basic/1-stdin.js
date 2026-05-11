6e2f08de79eeebc9de71452076218728a74614bf
alexandre-mouysset <12258@holbertonstudents.com>
2026-05-11 21:51:57 +0200

    fix: update stdin data handling for consistency in 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,8 @@
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (callback) => {
-  const input = callback.toString().trim();
+process.stdin.on('data', (data) => {
+  const input = data.toString().trim();
   console.log(`Your name is: ${input}`);
-});
-
-process.stdin.on('end', () => {
   console.log('This important software is now closing');
-})
+  process.exit();
+});
