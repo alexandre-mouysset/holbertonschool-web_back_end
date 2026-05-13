1fd6758a10ebd5f2171643ed6496184633f24def
alexandre-mouysset <12258@holbertonstudents.com>
2026-05-13 20:22:50 +0200

    style: format code for consistency and readability

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,23 +2,23 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-	if (req.url === '/') {
-		res.end('Hello Holberton School!');
-	} else if (req.url === '/students') {
-		const database = process.argv[2];
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    const database = process.argv[2];
 
-		countStudents(database)
-			.then(() => {
-				res.end('This is the list of our students');
-			})
-			.catch((err) => {
-				res.end(`This is the list of our students\n${err.message}`);
-			});
-	} else {
-		res.end('Hello Holberton School!');
-	}
+    countStudents(database)
+      .then(() => {
+        res.end('This is the list of our students');
+      })
+      .catch((err) => {
+        res.end(`This is the list of our students\n${err.message}`);
+      });
+  } else {
+    res.end('Hello Holberton School!');
+  }
 });
 
 module.exports = app;
